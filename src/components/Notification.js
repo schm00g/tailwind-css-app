ca013c10f3fdc5c39e2b3496e193aaeba74e8356
schm00g <hk8igureu@relay.firefox.com>
2022-03-18 13:40:44 +0000

    notify layout

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,15 +1,15 @@
 import logo from '../images/logo.svg';
 import React from 'react';
 
-export default function UtilityFirst(){
+export default function Notification(){
     return (
-        <div class="p-6 pr-10 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div class="p-6 mt-5 pr-10 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
             <div class="shrink-0">
                 <img class="h-12 w-12" src={logo} alt="ChitChat Logo" />
             </div>
             <div>
-            <div class="text-xl font-medium text-black">ChitChat</div>
-                <p class="text-slate-500 text-xl">You have a new message!</p>
+            <div class="text-base font-medium text-black">ChitChat</div>
+                <p class="text-slate-500 text-lg">You have a new message!</p>
             </div>
         </div> 
     )
